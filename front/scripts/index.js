318c5644a913a9e5f92e9229560ab8d1918920fb
c.molinab <cathemolinab@gmail.com>
2025-04-15 15:33:49 -0700

    avance

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,37 @@
-console.log(tempData);
+//console.log(tempData);
+
+class Activity {
+    constructor(id, title, description, imgUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+
+    tempData.map((movie) => {
+        const movieCard = document.createElement("div");
+        movieCard.classList.add("moviecard"); // Añadimos una clase para estilos CSS
+      
+        const poster = document.createElement("img");
+        poster.src = movie.poster;
+        poster.alt = movie.title;
+      
+        const title = document.createElement("h2");
+        title.textContent = movie.title;
+      
+        const details = document.createElement("div");
+        details.innerHTML = `
+          <p><strong>Año:</strong> ${movie.year}</p>
+          <p><strong>Director:</strong> ${movie.director}</p>
+          <p><strong>Duración:</strong> ${movie.duration}</p>
+          <p><strong>Género:</strong> ${movie.genre.join(", ")}</p>
+          <p><strong>Calificación:</strong> ${movie.rate}</p>
+        `;
+      
+        movieCard.appendChild(poster);
+        movieCard.appendChild(title);
+        movieCard.appendChild(details);
+      
+        movieContainer.appendChild(movieCard);
+      });
+      
