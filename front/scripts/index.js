66470a43290da8f6701244332609c0bbc0f32fcb
c.molinab <cathemolinab@gmail.com>
2025-04-16 06:49:28 -0700

    avance homework

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,37 +1,31 @@
 //console.log(tempData);
 
-class Activity {
-    constructor(id, title, description, imgUrl) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.imgUrl = imgUrl;
-    }
-
     tempData.map((movie) => {
         const movieCard = document.createElement("div");
-        movieCard.classList.add("moviecard"); // Añadimos una clase para estilos CSS
-      
-        const poster = document.createElement("img");
-        poster.src = movie.poster;
-        poster.alt = movie.title;
+        movieCard.classList.add("moviecard");
       
         const title = document.createElement("h2");
-        title.textContent = movie.title;
-      
-        const details = document.createElement("div");
-        details.innerHTML = `
-          <p><strong>Año:</strong> ${movie.year}</p>
-          <p><strong>Director:</strong> ${movie.director}</p>
-          <p><strong>Duración:</strong> ${movie.duration}</p>
-          <p><strong>Género:</strong> ${movie.genre.join(", ")}</p>
-          <p><strong>Calificación:</strong> ${movie.rate}</p>
+        title.textContent = `${movie.title}`;
+        
+        const poster = document.createElement("img");
+        poster.src = movie.poster; 
+        poster.alt = movie.title; 
+            
+        const description = document.createElement("div");
+        description.classList.add("moviedescription");
+        description.innerHTML = `
+          <p class="year">Año: ${movie.year}</p>
+          <p class="duration">Duración: ${movie.duration}</p>
+          <p class="genre">Género: ${movie.genre.join(", ")}</p>
+          <p class="rate">Calificación: ${movie.rate}</p>
         `;
-      
+
         movieCard.appendChild(poster);
         movieCard.appendChild(title);
-        movieCard.appendChild(details);
+        movieCard.appendChild(description);
       
         movieContainer.appendChild(movieCard);
+
+        return movieCard;
       });
       
