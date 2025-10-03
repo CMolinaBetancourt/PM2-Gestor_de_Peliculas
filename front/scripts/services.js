9f1690f1ff591ce1da5d685869ddc7e114603698
CMolinaBetancourt <cathemolinab@gmail.com>
2025-10-03 09:31:04 -0700

    fix/deploy

--- front/scripts/services.js
+++ front/scripts/services.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const renderCards = require('./renderCards');
+const API_BASE_URL= process.env.BACKDEPLOY;
 
 //$.get("https://students-api.up.railway.app/movies", (data) => {
 //  renderCards(data); 
@@ -11,7 +12,7 @@ const renderCards = require('./renderCards');
 
 const fetchMovies = async (container) => {
   try {
-    const { data } = await axios.get("http://localhost:3000/movies");
+    const { data } = await axios.get("${API_BASE_UR}/movies");
     const movies = data.movies;
     renderCards(movies, container);
     console.log('Películas cargadas:', movies);
@@ -22,7 +23,7 @@ const fetchMovies = async (container) => {
 
 const postMovie = async newMovie => {
   try {
-    const res = await axios.post('http://localhost:3000/movies', newMovie);
+    const res = await axios.post('${API_BASE_UR}/movies', newMovie);
     console.log('Película creada con éxito:', res);
     return res;
   } catch (err) {
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,9 +1,18 @@
+const path = require("path"); 
+const webpack = require("webpack");
+
 module.exports = {
-    entry: "./scripts/index.js",
-    output: {
-      path: __dirname + "/public",
-      filename: "bundle.js"
-    },
-    mode: "development",
-    watch: true,
-  };
+  entry: "./scripts/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  mode: "production",
+
+  plugins: [
+    new webpack.DefinePlugin({
+
+      "process.env.BACKDEPLOY": JSON.stringify(process.env.BACKDEPLOY || "")
+    })
+  ]
+};
